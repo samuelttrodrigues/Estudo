a90696b547ee8a4906c8aecbc080d0c40b1a0e66
samuel <samueltellesrodrigues@gmail.com>
2024-05-07 18:25:50 -0300

    Dia dois das aulas de Algoritmos

---
+++ Aulas teóricas Algoritmos/Aula teórica 3/if statements.py
@@ -0,0 +1,8 @@
+# Utilizando os termos Se, Se não entre outros para determinar para onde vamos
+
+x = int(input('Escreva seu primeiro número: '))
+y = int(input('Escreva seu segundo número: '))
+if (x > y):
+    print('Seu primeiro número é maior que o segundo!')
+if (x < y):
+    print('Seu segundo número é maior que o segundo')
