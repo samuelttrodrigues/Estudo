7182b44dac2d71c95e4a4a5f1194b5df53d3a78e
samuel <samueltellesrodrigues@gmail.com>
2024-05-06 22:41:19 -0300

    first commit

---
+++ Aulas praticas Algoritmos/Expressões algébricas.py
@@ -0,0 +1,40 @@
+#Expressões algébricas: Escrever as seguintes expressões algébricas em linguagem Python:
+# a) O somatório dos 5 primeiros números inteiros e positivos
+# b) A média entre 23, 19 e 31
+# c) O número de vezes que 73 cabe em 403
+# d) A sobra quando 403 é dividido por 73
+# e) 2 elevado à 10 potência
+# f) O valor absoluto da diferença entre 54 e 57
+# g) O menor valor entre 34, 29 e 31
+
+# a)
+somatorio = (0 + 1 + 2 + 3 + 4)
+print(somatorio)
+
+# b)
+media = (23 + 19 + 31) / 3
+print(media)
+
+# c)
+caber = (403 // 73)
+print(caber)
+
+# d)
+sobra = (403 % 73)
+print(sobra)
+
+# X) Teste de input
+#  teste = input("Qual é o seu nome?\n")
+#  print(f"Seu nome é {teste}")
+
+# e)
+elevado = (2 ** 10)
+print(elevado)
+
+# f)
+absoluto = (abs(54-57))
+print(absoluto)
+
+# g) 
+minimo = (min(34,29,31))
+print(minimo)
