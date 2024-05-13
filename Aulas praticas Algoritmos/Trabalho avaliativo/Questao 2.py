f85581533314f2f43573cef5787bf2850b8296f5
samuel <samueltellesrodrigues@gmail.com>
2024-05-12 22:51:38 -0300

    Questão 2 do trabalho completada

--- Aulas praticas Algoritmos/Trabalho avaliativo/Questao 2.py
+++ Aulas praticas Algoritmos/Trabalho avaliativo/Questao 2.py
@@ -6,15 +6,22 @@
 #?•	Tamanho G de Cupuaçu (CP) custa 18 reais e o Açaí (AC) custa 20 reais;
 
 
-print('Bem-vindo a gelateria do Samuel Thiago Telles Rodrigues.\n')
+print('Bem-vindo a sorveteria do balacobaco do Samuel Thiago Telles Rodrigues.\n')
 
+print('-'*23,' Cardápio ', '-'*23)
+print('-'*58)
+print('------|  Tamanho  |   Cupuaçu (CP) |   Açaí (AC)   |------')
+print('------|     P     |    R$  9.00    |   R$ 11.00    |------')
+print('------|     M     |    R$ 14.00    |   R$ 16.00    |------')
+print('------|     G     |    R$ 18.00    |   R$ 20.00    |------')
+print('-'*58)
 
-
+#Variável acumuladora Valor de base
 valor_final = 0
 
 while True:
                                                 #Escolha do sabor
-    sabor = input('Escolha o sabor. Cupuaçu ou Açaí. (CP/AC): ')
+    sabor = input('\nEscolha o sabor. Cupuaçu ou Açaí. (CP/AC): ')
     if (sabor != 'AC' and sabor != 'CP'):
         print('Sabor incorreto. Tente novamente.\n')
         continue
@@ -46,13 +53,19 @@ while True:
 
     print(f'Você pediu um {sabor} no tamanho {tamanho}: R${preco:.2f}\n')
 
+    # Variável acumuladora adicionando o preco para o valor final
     valor_final += preco
     
     outro = input('Deseja mais alguma coisa? (S/N): ')
 
     if (outro == 'S' or outro == 's'):
         continue
+
     elif (outro == 'N' or outro == 'n'):
+        break
+
+    else:   #Se não for nenhum desses inputs. Ira realizar o encerramento da compra e sera cobrado o valor final até o presente momento.
+        print('Erro inesperado... Finalizando sua compra até o momento')
         break
 
 print(f'\nValor total a ser pago: R$ {valor_final} \n')
